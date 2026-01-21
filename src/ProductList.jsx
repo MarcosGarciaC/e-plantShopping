b25bb84269a0c98b32ef7de5b14be64f6990a10a
MarcosGarciaC <marcos.garciacastillo459@gmail.com>
2026-01-20 18:01:45 -0600

    first batch finished

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+import { useDispatch } from 'react-redux';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({})
+    const dispatch = useDispatch();
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -252,6 +256,15 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
+        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
+            ...prevState, // Spread the previous state to retain existing entries
+            [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
+        }));
+    };
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -274,8 +287,30 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-
-
+                    {plantsArray.map((categoryGroup, index) => (
+                        <div key={index}>
+                            {/*Category Title*/}
+                            <h2>{categoryGroup.category}</h2>
+                            <div className='product-list'>
+                                {/*Next array for displaying plants by category*/}
+                                {categoryGroup.plants.map((plant, plantIndex) => (
+                                    <div key={plantIndex} className='product-card'>
+                                        <h3 className='product-title'>{plant.name}</h3>
+                                        <img className='product-image' src={plant.image} alt={plant.name} />
+                                        <p >{plant.description}</p>
+                                        <p className='product-price'>{plant.cost}</p>
+                                        <button
+                                            className="product-button"
+                                            onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
+                                            disabled={!!addedToCart[plant.name]}
+                                        >
+                                            {addedToCart[plant.name] ? 'Added' : 'Add to Cart'}
+                                        </button>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
